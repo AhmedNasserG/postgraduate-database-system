64d61a21054b768d9ccb4b1c5ba50029f611b1aa
ShimaaBetah <shimaabetah1911@gmail.com>
2021-12-23 11:41:34 +0200

    Add student register backend
    add student register function and connection for database in app.js

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
-
+const sql = require('mssql')
+const sqlConfig = {
+  user: 'linearDepression',
+  password: 'linearDepression12345',
+  database: 'pg_database',
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+const connect= async   () => {
+  try {
+ 
+   // make sure that any items are correctly URL encoded in the connection string
+   await sql.connect(sqlConfig)
+   const request = new sql.Request()
+ 
+  } catch (err) {
+   console.log(err.message)
+   
+  }
+ }
 // routes
 const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
@@ -29,6 +56,6 @@ app.use(express.static('public'));
 app.use('/', loginRoute);
 app.use('/register', registerRoute);
 
-app.listen(4000, () => {
+app.listen(4000, connect().then(() => {
   console.log('Server is running on port 4000');
-});
+}));
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var sql = require('mssql')
+const sqlConfig = {
+  user: 'linearDepression',
+  password: 'linearDepression12345',
+  database: 'pg_database',
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
+
+const studentRegister = async   (firstName, lastName, email, address, faculty, password,type) => {
+ try {
+
+  // make sure that any items are correctly URL encoded in the connection string
+  const request = new sql.Request()
+  request.input('first_name',sql.VarChar,firstName)
+  request.input('last_name',sql.VarChar,lastName)
+  request.input('password',sql.VarChar,password)
+  request.input('faculty',sql.VarChar,faculty)
+  request.input('Gucian',sql.Bit,type)
+  request.input('email',sql.VarChar,email)
+  request.input('address',sql.VarChar,address)
+  request.execute('StudentRegister')
+  //sql.close()
+ } catch (err) {
+  console.log(err.message)
+  
+ }
+}
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('register');
 });
+
+/*student Registration*/
+router.post('/student', function(req,res){
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName 
+  const email = req.body.email
+  const address = req.body.address
+  const faculty = req.body.faculty
+  const password = req.body.password
+  const type = req.body.type
+  console.log(req.body)
+  studentRegister(firstName,lastName,email,address,faculty,password,type)
+})
 
 module.exports = router;
