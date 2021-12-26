160c0263ca26778dde1f23db3021377511b28ef3
ahmednasserg <ahmednasser217217@gmail.com>
2021-12-26 13:59:28 +0200

    Add remaining admin procedures
    added `issueThesisPaymentInstallment`
    added `updateExtension`

--- procedures/adminProcedures.js
+++ procedures/adminProcedures.js
@@ -16,8 +16,38 @@ const numOfOnGoingTheses = async () => {
   return request.execute('AdminViewOnGoingTheses');
 };
 
+const issueThesisPayment = async (
+  thesis_serial_number,
+  amount,
+  num_of_installments,
+  fund_percentage
+) => {
+  const request = new sql.Request();
+  request.input('thesis_serial_number', sql.Int, thesis_serial_number);
+  request.input('amount', sql.Decimal, amount);
+  request.input('num_of_installments', sql.Int, num_of_installments);
+  request.input('fund_percentage', sql.Decimal, fund_percentage);
+  request.output('success', sql.Bit);
+  return request.execute('AdminIssueThesisPayment');
+};
+
+const issueThesisPaymentInstallment = async (payment_id, installment_date) => {
+  const request = new sql.Request();
+  request.input('payment_id', sql.Int, payment_id);
+  request.input('installment_date', sql.Date, installment_date);
+  return request.execute('AdminIssueInstallPayment');
+};
+
+const updateExtension = async thesis_serial_number => {
+  const request = new sql.Request();
+  request.input('thesis_serial_number', sql.Int, thesis_serial_number);
+  return request.execute('AdminUpdateExtension');
+};
+
 module.exports = {
   listSupervisors,
   listTheses,
-  numOfOnGoingTheses
+  numOfOnGoingTheses,
+  issueThesisPayment,
+  issueThesisPaymentInstallment
 };
