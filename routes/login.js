5a3bade207bc8fe14216f1d9a4135b18aa11d479
ahmednasserg <ahmednasser217217@gmail.com>
2021-12-29 14:10:22 +0200

    Add remember session feature in different tabs
    if user opened a new tab he is still logged in

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const userProcedures = require('../procedures/userProcedures');
-/* GET users listing. */
+
 router.get('/', function (req, res, next) {
-  res.render('login');
+  routeUser(req.session, res);
 });
 
 router.post('/', function (req, res) {
@@ -11,31 +11,13 @@ router.post('/', function (req, res) {
   const password = req.body.password;
   try {
     userProcedures.userLogin(email, password).then(response => {
-      console.log(response.output);
       if (response.output.success == false) {
         res.render('login', { verify: 'no' });
       } else {
         req.session.userId = response.output.id;
-        console.log(req.session.userId);
         userProcedures.userType(response.output.id).then(response => {
-          console.log(response.output.type);
-          if (response.output.type == 0) {
-            req.session.type = 0;
-            res.redirect('/student');
-          }
-          else if (response.output.type == 1) {
-            req.session.type = 1;
-            res.redirect('/supervisor');
-          } else if (response.output.type == 2) {
-            req.session.type = 2;
-            res.redirect('/examiner');
-          } else if (response.output.type == 3) {
-            req.session.type = 3;
-            res.redirect('/admin');
-          } else if (response.output.type == 4) {
-            req.session.type = 4
-            res.redirect('/student');
-          }
+          req.session.type = response.output.type;
+          routeUser(req.session, res);
         });
       }
     });
@@ -43,4 +25,19 @@ router.post('/', function (req, res) {
     console.log(error);
   }
 });
+
+function routeUser(session, res) {
+  if (session.type == 0 || session.type == 4) {
+    res.redirect('/student');
+  } else if (session.type == 1) {
+    res.redirect('/supervisor');
+  } else if (session.type == 2) {
+    res.redirect('/examiner');
+  } else if (session.type == 3) {
+    res.redirect('/admin');
+  } else {
+    res.render('login');
+  }
+}
+
 module.exports = router;
