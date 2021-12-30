a632fe977d5f0c98b29736f3f5a85b876c8ae5cc
ahmednasserg <ahmednasser217217@gmail.com>
2021-12-30 19:51:41 +0200

    Add authRole to auth utility
    added `authRole` function to check if user has permission to access to this page or not

--- utilities/auth.js
+++ utilities/auth.js
@@ -7,4 +7,15 @@ const authUser = function (req, res, next) {
   }
 };
 
-module.exports = { authUser };
+const authRole = function (role) {
+  return (req, res, next) => {
+    if (req.session.type == role) {
+      next();
+    } else {
+      res.status(401);
+      res.send('Unauthorized');
+    }
+  };
+};
+
+module.exports = { authUser, authRole };
