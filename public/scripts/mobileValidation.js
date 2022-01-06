21fc0da459f2dae1619a0ddc1a4c625ccdcf06f0
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2022-01-06 19:21:45 +0200

    [front] Add mobileValidation
    restrict the number to be in the Egyptian campiness.

--- public/scripts/mobileValidation.js
+++ public/scripts/mobileValidation.js
@@ -1,15 +1,17 @@
-  function validate(form) {
-      console.log('validate');
-      mobileField = document.getElementById('mobile');
-      // check if mobile is valid with regex
-      if (!mobileField.value.match(/^[0-9]{11}$/)) {
-        mobileField.setCustomValidity('Mobile number must be 11 digits');
-        mobileField.reportValidity();
-        mobileField.style.borderColor = 'red';
-        mobileField.focus();
-        return false;
-      }
-      mobileField.setCustomValidity('');
-      mobileField.style.borderColor = 'green';
-      return true;
-    }
+function validate(form) {
+  console.log('validate');
+  mobileField = document.getElementById('mobile');
+  // check if mobile is valid with regex
+  if (!mobileField.value.match(/^01[0125]([0-9]){8}$/)) {
+    mobileField.setCustomValidity(
+      'Mobile number must be 11 digits in the format 01[0125]XXXXXXXX'
+    );
+    mobileField.reportValidity();
+    mobileField.style.borderColor = 'red';
+    mobileField.focus();
+    return false;
+  }
+  mobileField.setCustomValidity('');
+  mobileField.style.borderColor = 'green';
+  return true;
+}
